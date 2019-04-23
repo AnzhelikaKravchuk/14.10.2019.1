ed1d60aa183855b0610ea27ba4d909c9362841c8
Arina <arinkarus@users.noreply.github.com>
2019-04-23 10:34:24 +0300

    Added implementation

--- PseudoEnumerable.Tests/EnumerableExtensionTests.cs
+++ PseudoEnumerable.Tests/EnumerableExtensionTests.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PseudoEnumerable;
 
 namespace PseudoEnumerable.Tests
 {
     public class EnumerableExtensionTests
     {
+        private static IEnumerable<TestCaseData> FilterTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(arg1: new int[] { 12, 7, 10, 0, 3, -8, -9 }, arg2: new EvenNumberPredicate(),
+                    arg3: new int[] { 12, 10, 0, -8 });
+            }
+        }
+
+        [Test, TestCaseSource(nameof(FilterTestCases))]
+        public void Filter_ConreteArrayAndPredicate_ReturnFilteredArray(int[] array, IPredicate<int> predicate, int[] expected)
+        {
+            CollectionAssert.AreEqual(array.Filter(predicate), expected);
+        }
+
+        [Test, TestCaseSource(nameof(FilterTestCases))]
+        public void Filter_Delegate_ReturnFilteredArray(int[] array, IPredicate<int> predicate, int[] expected)
+        {
+            CollectionAssert.AreEqual(array.Filter(x => x % 2 == 0), expected);
+        }
+
+        public bool IfEvenNumber(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public class EvenNumberPredicate : IPredicate<int>
+        {
+            /// <summary>
+            /// Check if number is even.
+            /// </summary>
+            /// <param name="number">Number to check.</param>
+            /// <returns>Returns true if number is even.</returns>
+            public bool IsMatching(int number)
+            {
+                return number % 2 == 0;
+            }
+        }
     }
 }
+
--- PseudoEnumerable/EnumerableExtension.cs
+++ PseudoEnumerable/EnumerableExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace PseudoEnumerable
@@ -10,6 +11,7 @@ namespace PseudoEnumerable
         {
             CheckOnNull(source);
             CheckOnNull(predicate);
+
             IEnumerable<TSource> GetFilteredItems()
             {
                 foreach (var item in source)
@@ -27,8 +29,7 @@ namespace PseudoEnumerable
         public static IEnumerable<TResult> Transform<TSource, TResult>(this IEnumerable<TSource> source,
             ITransformer<TSource, TResult> transformer)
         {
-            // Call EnumerableExtension.Transform with delegate
-            throw new NotImplementedException();
+            return source.Transform(transformer.Transform);
         }
 
         public static IEnumerable<TSource> SortBy<TSource>(this IEnumerable<TSource> source,
@@ -37,34 +38,74 @@ namespace PseudoEnumerable
             CheckOnNull(source);
             CheckOnNull(comparer);
             List<TSource> list = new List<TSource>(source);
-            return GetSorted();
 
             IEnumerable<TSource> GetSorted()
             {
                 list.Sort(comparer);
                 return list;
             }
+
+            return GetSorted();
         }
 
         public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source,
             Predicate<TSource> predicate)
         {
-            // Call EnumerableExtension.Filter with interface
-            throw new NotImplementedException();
+            return Filter<TSource>(source, new PredicateAdapter<TSource>(predicate));
         }
 
-        public static IEnumerable<TResult> Transform<TSource, TResult>(this IEnumerable<TSource> source,
-            Converter<TSource, TResult> transformer)
+        private class PredicateAdapter<T>: IPredicate<T>
         {
-            // Implementation Day 13 Task 1 (ArrayExtension)
-            throw new NotImplementedException();
+            private Predicate<T> method;
+
+            public bool IsMatching(T item)
+            {
+                return method.Invoke(item);
+            }
+
+            public PredicateAdapter(Predicate<T> predicate)
+            {
+                this.method = predicate;
+            }          
         }
 
         public static IEnumerable<TSource> SortBy<TSource>(this IEnumerable<TSource> source,
             Comparison<TSource> comparer)
         {
-            // Call EnumerableExtension.SortBy with interface
-            throw new NotImplementedException();
+            return source.SortBy(new SortByAdapter<TSource>(comparer));
+        }
+
+        private class SortByAdapter<TSource>: IComparer<TSource>
+        {
+            private Comparison<TSource> comparer;
+
+            public SortByAdapter(Comparison<TSource> comparer)
+            {
+                this.comparer = comparer;
+            }
+
+            public int Compare(TSource x, TSource y)
+            {
+               return this.comparer(x, y);
+            }
+        }
+
+        public static IEnumerable<TResult> Transform<TSource, TResult>(this IEnumerable<TSource> source,
+            Converter<TSource, TResult> transformer)
+        {
+            CheckOnNull(source);
+            CheckOnNull(transformer);
+
+            IEnumerable<TResult> GetItems()
+            {
+                foreach (var item in source)
+                {
+                    TResult value = transformer.Invoke(item);
+                    yield return value;
+                }
+            }
+
+            return GetItems();
         }
 
         private static void CheckOnNull(object obj)
