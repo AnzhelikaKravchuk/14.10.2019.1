d23d895cc13984a3a0f4ef69992b9c74dc7685d3
mtsyvis <mtsyvis2405@gmail.com>
2019-04-23 09:08:37 +0300

    Done first part

--- PseudoEnumerable/EnumerableExtension.cs
+++ PseudoEnumerable/EnumerableExtension.cs
@@ -8,22 +8,46 @@ namespace PseudoEnumerable
         public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source,
             IPredicate<TSource> predicate)
         {
-            // Implementation Day 13 Task 1 (ArrayExtension)
-            throw new NotImplementedException();
+            if (predicate == null)
+            {
+                yield break;
+            }
+
+            foreach (var i in source)
+            {
+                if (predicate.IsMatching(i))
+                {
+                    yield return i;
+                }
+            }
         }
 
         public static IEnumerable<TResult> Transform<TSource, TResult>(this IEnumerable<TSource> source,
             ITransformer<TSource, TResult> transformer)
         {
-            // Call EnumerableExtension.Transform with delegate
-            throw new NotImplementedException();
+            if (transformer == null)
+            {
+                yield break;
+            }
+
+            foreach (var i in source)
+            {
+                yield return transformer.Transform(i);
+            }
         }
 
         public static IEnumerable<TSource> SortBy<TSource>(this IEnumerable<TSource> source,
             IComparer<TSource> comparer)
         {
-            // Implementation Day 13 Task 1 (ArrayExtension)
-            throw new NotImplementedException();
+            if (comparer == null)
+            {
+                throw new ArgumentNullException($"comparer is null{nameof(comparer)}");
+            }
+
+            var copyArray = source.ToArray();
+            Array.Sort(copyArray, comparer);
+
+            return copyArray;
         }
 
         public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source,
@@ -45,6 +69,54 @@ namespace PseudoEnumerable
         {
             // Call EnumerableExtension.SortBy with interface
             throw new NotImplementedException();
+        }
+
+
+        private static TElement[] ToArray<TElement>(this IEnumerable<TElement> source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException($"{nameof(source)}");
+            }
+
+            TElement[] array = null;
+
+            if (source is ICollection<TElement> elements)
+            {
+                array = new TElement[elements.Count];
+                elements.CopyTo(array, 0);
+                return array;
+            }
+            else
+            {
+                var tempArray = new TElement[source.Count()];
+                int i = 0;
+                foreach (var element in source)
+                {
+                    tempArray[i] = element;
+                    i++;
+                }
+
+                array = new TElement[source.Count()];
+                Array.Copy(tempArray, array, tempArray.Length);
+                return array;
+            }
+        }
+
+        private static int Count<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException($"{nameof(source)}");
+            }
+
+            int count = 0;
+            while (source.GetEnumerator().MoveNext())
+            {
+                count++;
+            }
+
+            return count;
         }
     }
 }
