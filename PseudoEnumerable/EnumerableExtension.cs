93b3102a143fda02fd22f40cf664b7940d8ca7f7
maksfn <39927631+maksfn@users.noreply.github.com>
2019-04-23 09:09:22 +0300

    Update EnumerableExtension.cs

--- PseudoEnumerable/EnumerableExtension.cs
+++ PseudoEnumerable/EnumerableExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PseudoEnumerable
 {
@@ -8,8 +9,14 @@ namespace PseudoEnumerable
         public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source,
             IPredicate<TSource> predicate)
         {
-            // Implementation Day 13 Task 1 (ArrayExtension)
-            throw new NotImplementedException();
+            Validate(source);
+
+            if (predicate is null)
+            {
+                throw new ArgumentException($"{nameof(predicate)} cannot be null.");
+            }
+
+            return FilterCollection(source, predicate);
         }
 
         public static IEnumerable<TResult> Transform<TSource, TResult>(this IEnumerable<TSource> source,
@@ -22,8 +29,12 @@ namespace PseudoEnumerable
         public static IEnumerable<TSource> SortBy<TSource>(this IEnumerable<TSource> source,
             IComparer<TSource> comparer)
         {
-            // Implementation Day 13 Task 1 (ArrayExtension)
-            throw new NotImplementedException();
+            Validate(source);
+
+            var result = new List<TSource>(source);
+            result.Sort(comparer);
+
+            return ReturnSortBy(result);
         }
 
         public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source,
@@ -45,6 +56,33 @@ namespace PseudoEnumerable
         {
             // Call EnumerableExtension.SortBy with interface
             throw new NotImplementedException();
+        }
+
+        private static void Validate<TSource>(IEnumerable<TSource> dataSources)
+        {
+            if (dataSources is null)
+            {
+                throw new ArgumentNullException($"{nameof(dataSources)} cannot be null.");
+            }
+        }
+
+        private static IEnumerable<TSource> FilterCollection<TSource>(IEnumerable<TSource> numbers, IPredicate<TSource> predicate)
+        {
+            foreach (var item in numbers)
+            {
+                if (predicate.IsMatching(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        private static IEnumerable<TSource> ReturnSortBy<TSource>(IEnumerable<TSource> source)
+        {
+            foreach (var item in source)
+            {
+                yield return item;
+            }
         }
     }
 }
