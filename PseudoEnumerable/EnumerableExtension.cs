9f0f6f2f4f9096c7b25d15ec8652ddc0b5ee963e
nadzeay <39878698+nadzeay@users.noreply.github.com>
2019-04-23 09:15:47 +0300

    Update

--- PseudoEnumerable/EnumerableExtension.cs
+++ PseudoEnumerable/EnumerableExtension.cs
@@ -8,22 +8,75 @@ namespace PseudoEnumerable
         public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source,
             IPredicate<TSource> predicate)
         {
-            // Implementation Day 13 Task 1 (ArrayExtension)
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException($"The {nameof(source)} can not be NULL.");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException($"The {nameof(predicate)} cannot be NULL.");
+            }
+
+            List<TSource> list = new List<TSource>();
+
+            return Filter();
+
+            IEnumerable<TSource> Filter()
+            {
+                foreach (var value in source)
+                {
+                    if (predicate.IsMatching(value))
+                    {
+                        yield return value;
+                    }
+                }
+            }
         }
 
         public static IEnumerable<TResult> Transform<TSource, TResult>(this IEnumerable<TSource> source,
             ITransformer<TSource, TResult> transformer)
         {
-            // Call EnumerableExtension.Transform with delegate
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException($"The {nameof(source)} can not be NULL.");
+            }
+
+            if (transformer == null)
+            {
+                throw new ArgumentNullException($"The {nameof(transformer)} cannot be NULL.");
+            }
+
+            return Transform();
+
+            IEnumerable<TResult> Transform()
+            {
+                foreach (var item in source)
+                {
+                    yield return transformer.Transform(item);
+                }
+            }
         }
 
         public static IEnumerable<TSource> SortBy<TSource>(this IEnumerable<TSource> source,
             IComparer<TSource> comparer)
         {
-            // Implementation Day 13 Task 1 (ArrayExtension)
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException($"The {nameof(source)} can not be NULL.");
+            }
+
+            IComparer<TSource> _comparer = comparer ?? (typeof(IComparable<TSource>).IsAssignableFrom(typeof(TSource)) ? Comparer<TSource>.Default : null);
+
+            if (_comparer == null)
+            {
+                throw new ArgumentNullException($"The {nameof(comparer)} does not exist.");
+            }
+
+            TSource[] array = source.ToArray();
+            Array.Sort(array, _comparer);
+
+            return array;
         }
 
         public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source,
