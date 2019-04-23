b9bec9b2983c32b35fb911978158b4653eff4b49
Katsiaryna Lapotka <katy.lap99@gmail.com>
2019-04-23 09:07:30 +0300

    Realize SortBy Transform and Filter

--- PseudoEnumerable/EnumerableExtension.cs
+++ PseudoEnumerable/EnumerableExtension.cs
@@ -8,22 +8,51 @@ namespace PseudoEnumerable
         public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source,
             IPredicate<TSource> predicate)
         {
-            // Implementation Day 13 Task 1 (ArrayExtension)
-            throw new NotImplementedException();
+
+            ValidateIsNull(predicate, nameof(predicate));
+            ValidateIsNull(source, nameof(source));
+
+            foreach (var item in source)
+            {
+                if (!predicate.IsMatching(item))
+                {
+                    continue;
+                }
+
+                yield return item;
+            }
         }
 
         public static IEnumerable<TResult> Transform<TSource, TResult>(this IEnumerable<TSource> source,
             ITransformer<TSource, TResult> transformer)
         {
-            // Call EnumerableExtension.Transform with delegate
-            throw new NotImplementedException();
+            ValidateIsNull(source, nameof(source));
+            ValidateIsNull(transformer, nameof(transformer));
+
+            foreach (var item in source)
+            {
+                yield return transformer.Transform(item);
+            }
         }
 
         public static IEnumerable<TSource> SortBy<TSource>(this IEnumerable<TSource> source,
             IComparer<TSource> comparer)
         {
-            // Implementation Day 13 Task 1 (ArrayExtension)
-            throw new NotImplementedException();
+            ValidateIsNull(source, nameof(source));
+
+            if (comparer == null)
+            {
+                comparer = Comparer<TSource>.Default;
+            }
+
+            var list = new List<TSource>(source);
+
+            list.Sort();
+
+            foreach (var item in list)
+            {
+                yield return item;
+            }
         }
 
         public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source,
@@ -45,6 +74,19 @@ namespace PseudoEnumerable
         {
             // Call EnumerableExtension.SortBy with interface
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks if value is null.
+        /// </summary>
+        /// <param name="array">The collection to check.</param>
+        /// <exception cref="ArgumentNullException">Throws when collection is null.</exception>
+        private static void ValidateIsNull<T>(T value, string name)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException($"{name} cannot be null");
+            }
         }
     }
 }
