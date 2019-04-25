2508f4581a2ebc2d32b9f9e07b7a2730492f39bd
Stepanovich Vladislav <stepanovich.98@mail.ru>
2019-04-25 23:40:59 +0300

    done

--- PseudoEnumerable.Tests/EnumerableExtensionTests.cs
+++ PseudoEnumerable.Tests/EnumerableExtensionTests.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace PseudoEnumerable.Tests
 {
     public class EnumerableExtensionTests
     {
+        [TestCase(new int[] { 1, 4, 3, 2, 5, 6 }, ExpectedResult = new int[] { 2, 4, 6, 1, 3, 5 })]
+        public IEnumerable<int> SortBy_FirstEven_returnsSortedSequence(int[] mas)
+        {
+            return mas.SortBy((x,y) => (x % 2 == 0 && y % 2 != 0) ? -1 : x - y);
+        }
     }
 }
--- PseudoEnumerable/EnumerableExtension.cs
+++ PseudoEnumerable/EnumerableExtension.cs
@@ -30,8 +30,21 @@ namespace PseudoEnumerable
         public static IEnumerable<TResult> Transform<TSource, TResult>(this IEnumerable<TSource> source,
             ITransformer<TSource, TResult> transformer)
         {
-            // Call EnumerableExtension.Transform with delegate
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} cannot be null");
+            }
+
+            if (transformer == null)
+            {
+                throw new ArgumentNullException($"{nameof(transformer)} cannot be null");
+            }
+
+            foreach (var item in source)
+            {
+                yield return transformer.Transform(item);
+            }
+            //return Transform<TSource, TResult>(source, transformer.Transform);
         }
 
         public static IEnumerable<TSource> SortBy<TSource>(this IEnumerable<TSource> source,
@@ -56,22 +69,129 @@ namespace PseudoEnumerable
         public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source,
             Predicate<TSource> predicate)
         {
-            // Call EnumerableExtension.Filter with interface
-            throw new NotImplementedException();
+
+            CheckForExceptionsPredicate(source,predicate);
+
+            //foreach (var item in source)
+            //{
+            //    if (predicate(item))
+            //    {
+            //        yield return item;
+            //    }
+            //}
+            
+            return source.Filter(new AdapterForFilter<TSource>(predicate));       
         }
 
         public static IEnumerable<TResult> Transform<TSource, TResult>(this IEnumerable<TSource> source,
             Converter<TSource, TResult> transformer)
         {
-            // Implementation Day 13 Task 1 (ArrayExtension)
-            throw new NotImplementedException();
+            CheckForExceptionsConverter(source, transformer);
+
+            //foreach (var item in source)
+            //{
+            //    yield return transformer(item);
+            //}
+            
+           return source.Transform(new AdapterForTransform<TSource,TResult>(transformer));
         }
 
         public static IEnumerable<TSource> SortBy<TSource>(this IEnumerable<TSource> source,
             Comparison<TSource> comparer)
         {
-            // Call EnumerableExtension.SortBy with interface
-            throw new NotImplementedException();
+            CheckForExceptionsComparison(source, comparer);
+            //List<TSource> sourceList = new List<TSource>(source);
+            //foreach (var item in source)
+            //{
+            //    sourceList.Sort(comparer);
+            //}
+
+            //return sourceList;
+            return source.SortBy(new AdapterForSortBy<TSource>(comparer));
+        }
+
+        private static void CheckForExceptionsComparison<TSource>(IEnumerable<TSource> source,
+            Comparison<TSource> comparer)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} can't b null");
+            }
+
+            if (comparer == null)
+            {
+                throw new ArgumentNullException($"{nameof(comparer)} can't b null");
+            }
+        }
+
+        private static void CheckForExceptionsPredicate<TSource>(IEnumerable<TSource> source,
+          Predicate<TSource> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} can't b null");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException($"{nameof(predicate)} can't b null");
+            }
+        }
+
+        private static void CheckForExceptionsConverter<TSource,TResult>(IEnumerable<TSource> source,
+         Converter<TSource,TResult> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} can't b null");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException($"{nameof(predicate)} can't b null");
+            }
+        }
+    }
+    class AdapterForFilter<T> : IPredicate<T>
+    {
+        Predicate<T> Predicate { get; set; }
+        public AdapterForFilter(Predicate<T> predicate)
+        {
+            Predicate = predicate;
+        }
+
+        public bool IsMatching(T item)
+        {
+            return Predicate(item);
+        }
+    }
+
+    class AdapterForTransform<T1, T2> : ITransformer<T1, T2>
+    {
+        Converter<T1, T2> Converter { get; set; }
+        public AdapterForTransform(Converter<T1, T2> convert)
+        {
+            Converter = convert;
+        }
+
+        public T2 Transform(T1 item)
+        {
+            return Converter(item);
+        }
+    }
+
+    class AdapterForSortBy<T> : IComparer<T>
+    {
+        Comparison<T> Comparison { get; set; }
+
+        public AdapterForSortBy(Comparison<T> comparison)
+        {
+            Comparison = comparison;
+        }
+
+        public int Compare(T x, T y)
+        {
+            return Comparison(x, y);
         }
     }
 }
